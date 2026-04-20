b396bdbda1027f5cee0124afec18c606e9d68a55
asaphfelix <asaphfelix@example.com>
2026-04-20 15:43:34 +0000

    Supprimer bouton Passer commande et garder commande via WhatsApp

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import checkout, confirmation
+from .views import confirmation
 
 urlpatterns = [
-    path("checkout/", checkout, name="checkout"),
     path("checkout/confirmation/<int:order_id>/", confirmation, name="order_confirmation"),
 ]
