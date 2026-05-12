740cdc04c4b5e2543363f8e4e9cd870394b6437c
asaphfelix <asaphfelix@example.com>
2026-05-12 19:26:00 +0000

    Suppression des mentions de livraison et paiement à la livraison

--- doriane_world/settings.py
+++ doriane_world/settings.py
@@ -9,8 +9,8 @@ env = environ.Env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "change-me")
-DEBUG = os.getenv("DJANGO_DEBUG", "True").lower() == "true"
-ALLOWED_HOSTS = ["doriane.pythonanywhere.com", "localhost", "127.0.0.1",
+DEBUG = os.getenv("DJANGO_DEBUG", "False").lower() == "true"
+ALLOWED_HOSTS = ["doriane.pythonanywhere.com", "localhost", "127.0.0.1","doriane-world.com"
 ] + [
     host.strip()
     for host in os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
